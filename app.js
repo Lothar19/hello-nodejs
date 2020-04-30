3dcaa2127940a4d931fdbc989261424d3fa62dfb
Lothar19 <Lothar19_@hotmail.com>
2020-04-30 06:03:28 +0200

    new features

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log("Hello World 4!!!!!\n"); 
+
+var express = require('express');
+app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello New World!\n');
+});
+
+app.get('/mars', function(req, res) {
+    res.send('Hello Mars!\n');
+});
+
+app.listen(8081, function() {
+    console.log("Example app listening on port 8081!\n"); 
+});
